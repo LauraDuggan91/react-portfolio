64d05687920bf2ae2fccb80460cb67bcdda3ca54
Laura Duggan <lauracatherineduggan@gmail.com>
2023-01-25 23:30:45 -0500

    background images (#2)

--- src/Pages/DesignPanel.js
+++ src/Pages/DesignPanel.js
@@ -6,11 +6,11 @@ import NailDate from '../assets/NailDate.png'
 import NewToronto from '../assets/NewToronto.png'
 import NickelsonHomes from '../assets/NickelsonHomes.png'
 import MySkdule from '../assets/MySkdule.png'
-import Calendar from '../assets/Calendar.jpg'
-import Renovation from '../assets/Renovation.jpg'
-import Tech from '../assets/Tech.jpg'
-import Trees from '../assets/Trees.jpg'
-import Nails from '../assets/Nails.jpg'
+// import Calendar from '../assets/Calendar.jpg'
+// import Renovation from '../assets/Renovation.jpg'
+// import Tech from '../assets/Tech.jpg'
+// import Trees from '../assets/Trees.jpg'
+// import Nails from '../assets/Nails.jpg'
 import { Link } from 'react-router-dom';
 import { FaExternalLinkAlt } from 'react-icons/fa';
 
@@ -32,21 +32,21 @@ const imageName = (image) => {
     }
 }
 
-const backgroundImage = (image) => {
-    switch(image) {
-        case 'EntryManager':
-            return `linear-gradient(rgba(93, 34, 34,0.9), rgba(21,21,21,0.9)), url(${Tech})`
-        case 'NailDate':
-            return `linear-gradient(rgba(78, 28, 56,0.9), rgba(21,21,21,0.9)), url(${Nails})`
-        case 'NewToronto':
-            return `linear-gradient(rgba(32, 74, 74,0.9), rgba(21,21,21,0.9)), url(${Trees})`
-        case 'NickelsonHomes':
-            return `linear-gradient(rgba(70, 60, 52,0.9), rgba(21,21,21,0.9)), url(${Renovation})`
-        case 'MySkdule':
-            return `linear-gradient(rgba(63, 34, 93,0.9), rgba(21,21,21,0.9)), url(${Calendar})`
-        default:
-    }
-}
+// const backgroundImage = (image) => {
+//     switch(image) {
+//         case 'EntryManager':
+//             return `linear-gradient(rgba(93, 34, 34,0.9), rgba(21,21,21,0.9)), url(${Tech})`
+//         case 'NailDate':
+//             return `linear-gradient(rgba(78, 28, 56,0.9), rgba(21,21,21,0.9)), url(${Nails})`
+//         case 'NewToronto':
+//             return `linear-gradient(rgba(32, 74, 74,0.9), rgba(21,21,21,0.9)), url(${Trees})`
+//         case 'NickelsonHomes':
+//             return `linear-gradient(rgba(70, 60, 52,0.9), rgba(21,21,21,0.9)), url(${Renovation})`
+//         case 'MySkdule':
+//             return `linear-gradient(rgba(63, 34, 93,0.9), rgba(21,21,21,0.9)), url(${Calendar})`
+//         default:
+//     }
+// }
 
 
 const DesignPanel = () => {
